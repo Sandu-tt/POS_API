8df6981b8c6c17d453c0eadcdde897e4f53642dd
Sandu <tharakasanu80@gmail.com>
2024-05-21 01:19:03 +0530

    added ProductSchema.js

---
+++ model/ProductSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+    },
+    description: {
+        type:String,
+        required:true,
+    },
+   unitPrice: {
+        type:Number,
+        required:true,
+    },
+
+    qtyOnHand: {
+        type:Number,
+        required:true,
+    },
+});
+
+module.exports=mongoose.model('product', ProductSchema);
